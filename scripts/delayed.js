fc86eee7767414e24602d4f05caa621b3fe57137
Abhilash Mohapatra <abhilash.a.mohapatra@accenture.com>
2024-04-30 18:45:15 +0530

    iframe append ecid

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -127,7 +127,7 @@ function set_ecid(){
   const iframeBlock = document.getElementById('bmwIframe');
   if(iframeBlock){
     const anchor = iframeBlock.src || '';
-    alloy('appendIdentityToUrl', { url: anchor }).then(result => {document.location = result.url;});
+    alloy('appendIdentityToUrl', { url: anchor }).then(result => {iframeBlock.src = result.url;});
   }
 }
 analyticsTracking();
