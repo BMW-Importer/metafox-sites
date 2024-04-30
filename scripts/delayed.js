2ed6ce2b7e78900950a5a13252fa0a1e884a5958
Abhilash Mohapatra <abhilash.a.mohapatra@accenture.com>
2024-04-30 14:42:16 +0530

    Ecid function added

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -2,11 +2,89 @@
 import { sampleRUM } from './aem.js';
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
+const page_tracking = {"page": {
+        "pageInfo": {
+            "pageID": "/content/bmw/marketB4R1/bmw_rs/sr_RS/index",
+            "version": "acdl: 2024-03-27t12: 24: 35.759+01: 00",
+            "siteSection": "cars",
+            "breadCrumbs": [
+                "home",
+                "cars",
+                "models",
+                "modelX"
+            ],
+            "sysEnv": "desktop",
+            "destinationURL": "https://www.bmw.rs/sr/index.html",
+            "referringURL": "",
+            "variant": "real page",
+            "geoRegion": "RS",
+            "language": "sr",
+            "websiteEnv": "prod",
+            "pageTitle": "BMW Srbija",
+            "pageName": "web:home",
+            "windowInfo": {
+                "screenWidth": 3840,
+                "screenHeight": 2160,
+                "screenOrientation": "landscape",
+                "userAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+                "server": "www.bmw.rs",
+                "url": "https://www.bmw.rs/sr/index.html",
+                "previousDomain": "",
+                "campaign": "",
+                "internalCampaign": "",
+                "urlClean": "https://www.bmw.rs/sr/index.html",
+                "queryParam": ""
+            },
+            "timeInfo": {
+                "localTime": "20:43:11",
+                "utcTime": "18:43:11"
+            }
+        },
+        "category": {
+            "primaryCategory": "central car stock locator",
+            "subCategory1": "",
+            "subCategory2": "",
+            "subCategory3": "",
+            "pageType": "search"
+        },
+        "campaign": {
+            "trackingCode": "utm_source=soc&utm_medium=twit&utm_campaign=MU_BMW_Brand_SOCIAL_AWNS&utm_id=HSZ2Q1BH7M9T&utm_term=brnd&utm_content=miy",
+            "campaignSource": "soc",
+            "campaignMedium": "twit",
+            "campaignName": "MU_BMW_Brand_SOCIAL_AWNS",
+            "campaignID": "HSZ2Q1BH7M9T",
+            "campaignTerm": "pro",
+            "campaignContent": "miy"
+        },
+        "attributes": {
+            "parentDomain": ".bmw.rs",
+            "brand": "bmw",
+            "outletID": "1234_1",
+            "outletName": "Nelson's Autohaus (Thailand) Co., L",
+            "dealerID": "1234",
+            "dealerName": "Nelson's Autohaus (Thailand) Co., L"
+        }
+    },
+    "eventInfo": {
+        "id": "2121221",
+        "eventName": "pageview",
+        "timeStamp": 1712774591731
+    },
+    "event": "pageview",
+    "user": {
+        "loginStatus": "logged_out",
+        "consent": {
+            "analytics": true,
+            "marketing": true,
+            "personalization": false
+        }
+    }}
 
 // add more delayed functionality here
 function analyticsTracking() {
     opt_in_info();
     set_page_tracking();
+    set_ecid();
 }
 function opt_in_info(){
   const adobeDtm = window.adobeDataLayer;
@@ -28,85 +106,18 @@ function opt_in_info(){
   });
 }
 function set_page_tracking(){
-    window.adobeDataLayer.push({
-  page: {
-    pageInfo: {
-      pageID: "/content/bmw/marketB4R1/bmw_rs/sr_RS/index",
-      version: "acdl: 2024-03-27t12: 24: 35.759+01: 00",
-      siteSection: "cars",
-      breadCrumbs: [
-        "home",
-        "cars",
-        "models",
-        "modelX"
-      ],
-      sysEnv: "desktop",
-      destinationURL: "https://www.bmw.rs/sr/index.html",
-      referringURL: "",
-      variant: "real page",
-      geoRegion: "RS",
-      language: "sr",
-      websiteEnv: "prod",
-      pageTitle: "BMW Srbija",
-      pageName: "web:home",
-      windowInfo: {
-        screenWidth: 3840,
-        screenHeight: 2160,
-        screenOrientation: "landscape",
-        userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
-        server: "www.bmw.rs",
-        url: "https://www.bmw.rs/sr/index.html",
-        previousDomain: "",
-        campaign: "",
-        internalCampaign: "",
-        urlClean: "https://www.bmw.rs/sr/index.html",
-        queryParam: ""
-      },
-      timeInfo: {
-        localTime: "20:43:11",
-        utcTime: "18:43:11"
-      }
-    },
-    category: {
-      primaryCategory: "central car stock locator",
-      subCategory1: "",
-      subCategory2: "",
-      subCategory3: "",
-      pageType: "search"
-    },
-    campaign: {
-      trackingCode: "utm_source=soc&utm_medium=twit&utm_campaign=MU_BMW_Brand_SOCIAL_AWNS&utm_id=HSZ2Q1BH7M9T&utm_term=brnd&utm_content=miy",
-      campaignSource: "soc",
-      campaignMedium: "twit",
-      campaignName: "MU_BMW_Brand_SOCIAL_AWNS",
-      campaignID: "HSZ2Q1BH7M9T",
-      campaignTerm: "pro",
-      campaignContent: "miy"
-    },
-    attributes: {
-      parentDomain: ".bmw.rs",
-      brand: "bmw",
-      outletID: "1234_1",
-      outletName: "Nelson's Autohaus (Thailand) Co., L",
-      dealerID: "1234",
-      dealerName: "Nelson's Autohaus (Thailand) Co., L"
-    }
-  },
-  eventInfo: {
-    id: "2121221",
-    eventName: "pageview",
-    timeStamp: 1712774591731
-  },
-  event: "pageview",
-  user: {
-    loginStatus: "logged_out",
-    consent: {
-      analytics: true,
-      marketing: true,
-      personalization: false
-    }
-  }
-})
-}
+  // adding page tracking properties
+    page_tracking.page.pageInfo.windowInfo.screenWidth = window.screen.width;
+    page_tracking.page.pageInfo.windowInfo.screenHeight = window.screen.height;
+    page_tracking.page.pageInfo.windowInfo.screenOrientation = window.screen.orientation.type;
+    page_tracking.page.pageInfo.windowInfo.userAgent = navigator.userAgent;
+    page_tracking.page.pageInfo.windowInfo.url = window.location.href;
 
+
+    window.adobeDataLayer.push(page_tracking);
+}
+function set_ecid(){
+  const anchor = document.getElementById('bmwIframe').src;
+  alloy('appendIdentityToUrl', { url: anchor }).then(result => {document.getElementById('bmwIframe').src = result.url;});
+}
 analyticsTracking();
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -150,8 +150,6 @@ function launchVariables() {
   const scriptElement1 = document.createElement('script');
   scriptElement1.setAttribute('src', 'https://assets.adobedtm.com/413a8cbe910e/2a9212d4511b/launch-6ca074b36c7e-development.min.js');
   headElement.appendChild(scriptElement1);
-  // const anchor = document.getElementById('bmwIframe').src;
-  // const url = new URL(anchor);
 }
 
 async function loadPage() {
