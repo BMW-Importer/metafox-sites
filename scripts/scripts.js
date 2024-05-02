c69ff7bca5c729c07346c5df9f8000a9cf9c4e8f
Harish-DV <163002991+Harish-DV@users.noreply.github.com>
2024-05-02 07:10:16 +0200

    Updated optin initially on scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -156,8 +156,28 @@ async function loadPage() {
   await loadEager(document);
   await loadLazy(document);
   await launchVariables();
+  await opt_in_info();
   loadDelayed();
-  // analyticsTracking();
+}
+
+function opt_in_info(){
+  const adobeDtm = window.adobeDataLayer;
+  console.log(adobeDtm.version);
+  const d = new Date();
+  alloy('setConsent', {
+    consent: [{
+      standard: 'Adobe',
+      version: '2.0',
+      value: {
+        collect: {
+          val: 'y'
+        },
+        metadata: {
+          time: '2024-04-30T07:00:05-7:00'
+        }
+      }
+    }]
+  });
 }
 
 loadPage();
