358413ac718ff7ebd8ebb2ee0504f33e997d4fa7
Manish <manish.cn.kumar@accenture.com>
2024-06-11 10:38:29 +0530

    js added for analytics

--- blocks/text-with-media/text-with-media.js
+++ blocks/text-with-media/text-with-media.js
@@ -79,7 +79,9 @@ export default function decorate(block) {
     const [classes, content, media, cta, analytics] = childrenBlockProps.children;
     // removing classes div
     const [analyticsLabel, analyticsBtnValue, analyticsOther] = analytics.children;
+
     childrenBlockProps.removeChild(classes);
+
     const imageAlignment = classes?.textContent?.split(',')?.[1] || 'left';
     childrenBlockProps.classList.add(imageAlignment?.trim());
     let mediaType;
@@ -109,8 +111,9 @@ export default function decorate(block) {
       ctaElem?.classList?.add('text-with-media-cta');
       if (analytics.children) {
         ctaElem.dataset.analyticsLabel = analyticsLabel.textContent;
-        ctaElem.dataset.buttonLink = analyticsBtnValue.textContent || analyticsOther.textContent;
+        ctaElem.dataset.buttonLink = analyticsBtnValue.textContent !== 'Other' ? analyticsBtnValue.textContent : analyticsOther.textContent;
         ctaElem.dataset.analyticsCustomeClick = 'true';
+        ctaElem.dataset.analyticsBlockName = ctaElem.closest('.block').dataset.blockName;
       }
       content.append(ctaElem);
     }
