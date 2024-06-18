40210c69194c8636113af3967c4706ee30470ca6
Babita Sahu <babita.sahu@accenture.com>
2024-06-18 13:32:55 +0530

    emptying panel

--- blocks/media-carousel/media-carousel.js
+++ blocks/media-carousel/media-carousel.js
@@ -353,7 +353,7 @@ export default function decorate(block) {
   // loop through all children blocks
     [...panels].forEach((panel, index) => {
       const [content, media, cta] = panel.children;
-
+      panel.textContent = '';
       if (media?.children?.length > 1) {
         // Create a wrapper for video card elements
         const videoCarouselCard = document.createElement('div');
@@ -481,7 +481,6 @@ export default function decorate(block) {
         videoImageCarouselContent.append(imageCarouselCard);
       }
     });
-    block.textContent = '';
     block.append(carouselLeftWrapper, carouselRightWrapper);
     block.append(videoImageCarouselContent);
     resizeBlock();
