1b2f823dc080185ec7e2b70789fa72aa3b5e983b
resmaacharyaaccenture <resma.acharya@accenture.com>
2024-06-20 18:44:51 +0530

    download analytics changes

--- blocks/download/download.js
+++ blocks/download/download.js
@@ -1,4 +1,10 @@
 export default function decorate(block) {
+  //console.log(block)
   const anchorEle = block.querySelector('a');
-  if (anchorEle) anchorEle.target = '_blank';
+  if (anchorEle){
+    anchorEle.target = '_blank';
+    anchorEle.dataset.analyticsCustomClick = 'true';
+    anchorEle.dataset.analyticsBlockName = anchorEle.closest('.block').dataset.blockName;
+    anchorEle.dataset.analyticsSectionId = anchorEle.closest('.section').dataset.analyticsLabel;
+  }
 }
