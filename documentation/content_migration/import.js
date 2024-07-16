9a5f7c607618abd37cbdcf1073ba64dbfdf34c43
Nikita Aggarwal (ext.) <nikita.b.aggarwal@accenture.com>
2024-07-16 15:03:25 +0530

    updated script for background media and video

--- documentation/content_migration/import.js
+++ documentation/content_migration/import.js
@@ -63,7 +63,7 @@ export default {
 };
 
 const createBackgroundMedia = (main, document) => {
-
+ 
     const listOfBackgroundMedia = document.querySelectorAll('.backgroundmedia');
     listOfBackgroundMedia.forEach(bgmedia => {
         const backgroundMediaBlock = [];
@@ -71,7 +71,7 @@ const createBackgroundMedia = (main, document) => {
         backgroundMediaBlock.push(name);
         const backgroundMediaBlockData = [];
         const backgroundMediaBlockData1 = [];
-
+ 
         const titles = bgmedia.querySelectorAll('.title');
         titles.forEach((title, i) => {
             if (i == 0) {
@@ -82,21 +82,21 @@ const createBackgroundMedia = (main, document) => {
                     const h5 = document.createElement('h5');
                     h5.textContent = eyeBrow;
                     backgroundMediaBlockData1.push(h5);
-
+ 
                 } else if (title.classList.contains('style-title__text--eyebrow1-bold')) {
                     eyeBrowType = "Eyebrow Bold 1 (h4)";
                     const h4 = document.createElement('h4');
                     h4.textContent = eyeBrow;
                     backgroundMediaBlockData1.push(h4);
-                } else if (title.classList.contains('style-title--iconization-1')) {
+                } else  {
                     eyeBrowType = 'Iconization (h6)';
                     const h6 = document.createElement('h6');
                     h6.textContent = eyeBrow;
                     backgroundMediaBlockData1.push(h6);
                 }
-
+ 
             }
-
+ 
             if (i == 1) {
                 const headline = title.querySelector('.cmp-title__text')?.textContent;
                 const para = document.createElement("p");
@@ -107,9 +107,9 @@ const createBackgroundMedia = (main, document) => {
                 h2.textContent = "big";
                 backgroundMediaBlockData1.push(h2);
                 backgroundMediaBlockData.push(backgroundMediaBlockData1);
-
+ 
             }
-
+ 
             if (i == 2) {
                 const copyText = title.querySelector('.cmp-title__text')?.textContent;
                 let subBrandIconType = title.querySelector('.cmp-title__image-branding')?.getAttribute('title');
@@ -120,14 +120,39 @@ const createBackgroundMedia = (main, document) => {
                 } else {
                     subBrandIconType = 'noicon';
                 }
-
-
                 if (copyText)
                     backgroundMediaBlockData.push(subBrandIconType);
-                backgroundMediaBlockData.push(copyText);
+                    backgroundMediaBlockData.push(copyText);
             }
         });
+ 
+   
+        let mediaImagetitle;
+        let mediaImageDescription;
+       
+ 
+        const mediaImageCmp = bgmedia.querySelector('.cmp-image .cmp-image__image');
+        if (mediaImageCmp) {
+            mediaImagetitle = bgmedia.querySelector('.cmp-title .cmp-title__text')?.textContent;
+            if(mediaImagetitle) {
+                let subBrandIconType = bgmedia.querySelector('.cmp-title__image-branding')?.getAttribute('title');
+                if (subBrandIconType && subBrandIconType.toLowerCase() == 'bmw i') {
+                        subBrandIconType = 'subbrandi';
+                } else if (subBrandIconType && subBrandIconType.toLowerCase() == 'bmw m') {
+                    subBrandIconType = 'subbrandm';
+                } else {
+                    subBrandIconType = 'noicon';
+                }
+                backgroundMediaBlockData.push(mediaImagetitle);
+				backgroundMediaBlockData.push(subBrandIconType);
 
+            }
+            mediaImageDescription = bgmedia.querySelector('.cmp-text .cmp-text__paragraph')?.textContent;
+            if(mediaImageDescription) {
+                backgroundMediaBlockData.push(mediaImageDescription);
+            }
+        }
+ 
         const backgroundMediaBlockData2 = [];
         const para = document.createElement("p");
         const node = document.createTextNode("dark,left");
@@ -137,39 +162,45 @@ const createBackgroundMedia = (main, document) => {
         h3.textContent = "central-left";
         backgroundMediaBlockData2.push(h3);
         backgroundMediaBlockData.push(backgroundMediaBlockData2);
-
+ 
         if (bgmedia.querySelector('.cmp-video__video-player')) {
             const backgroundMediaVideoBlockData = [];
-
+ 
             const videoTitle = bgmedia.querySelector('.cmp-video__video-player').getAttribute('aria-label');
             const paraTitle = document.createElement("p");
             const nodeTitle = document.createTextNode(videoTitle);
+            if(videoTitle)
             paraTitle.appendChild(nodeTitle);
             const videoDescription = bgmedia.querySelector('.cmp-video__video-player').getAttribute('aria-description');
             const paraDesc = document.createElement("p");
             const nodeDesc = document.createTextNode(videoDescription);
+            if(videoDescription)
             paraDesc.appendChild(nodeDesc);
             const dsktpPosterImgPath = bgmedia.querySelector('.cmp-video__video img')?.getAttribute('src');
             const para3 = document.createElement("p");
             const el = document.createElement('img');
             el.src = dsktpPosterImgPath;
+            if(dsktpPosterImgPath)
             para3.appendChild(el);
             const mblPosterImgPath = bgmedia.querySelector('.cmp-video__video img')?.getAttribute('src');
             const para4 = document.createElement("p");
             const e2 = document.createElement('img');
             e2.src = mblPosterImgPath;
+            if(mblPosterImgPath)
             para3.appendChild(e2);
             const desktopVideoPath = bgmedia.querySelector('.cmp-video__video-player').getAttribute('data-src-large');
             const para5 = document.createElement("p");
             const node5 = document.createTextNode(desktopVideoPath);
+            if(desktopVideoPath)
             para5.appendChild(node5);
             let mobileVideoPath = bgmedia.querySelector('.cmp-video__video-player').getAttribute('data-src-medium');
             if (!mobileVideoPath) {
                 mobileVideoPath = bgmedia.querySelector('.cmp-video__video-player').getAttribute('src');
             }
-
+ 
             const para6 = document.createElement("p");
             const node6 = document.createTextNode(mobileVideoPath);
+            if(mobileVideoPath)
             para6.appendChild(node6);
             const h1 = document.createElement('h1');
             h1.textContent = "true";
@@ -177,12 +208,15 @@ const createBackgroundMedia = (main, document) => {
             h2.textContent = "true";
             backgroundMediaVideoBlockData.push(paraTitle, paraDesc, para3, para4, para5, para6, h1, h2);
             backgroundMediaBlockData.push(backgroundMediaVideoBlockData);
-
-
+ 
+ 
         }
-
+ 
         let imgSrc;
         let imgAlt
+       
+       
+ 
         const mediaImage = bgmedia.querySelector('.cmp-image .cmp-image__image');
         if (mediaImage) {
             imgSrc = bgmedia.querySelector('.cmp-image .cmp-image__image')?.getAttribute('src');
@@ -190,14 +224,24 @@ const createBackgroundMedia = (main, document) => {
             const el = document.createElement('img');
             el.src = imgSrc;
             el.alt = imgAlt;
+            if(imgSrc)
             backgroundMediaBlockData.push(el);
         }
-
+ 
+        function isAbsoluteUrl(url) {
+            return /^https?:\/\//i.test(url);
+        }
+ 
         const linkLabel1 = bgmedia.querySelector('.button .cmp-button[data-tracking-linkid*="primary button"]')?.textContent;
         const linkLabel2 = bgmedia.querySelector('.button .cmp-button[data-tracking-linkid*="ghost button"]')?.textContent;
-        const linkurl1 = bgmedia.querySelector('.button .cmp-button[data-tracking-linkid*="primary button"]')?.getAttribute('href');
-        const linkurl2 = bgmedia.querySelector('.button .cmp-button[data-tracking-linkid*="ghost button"]')?.getAttribute('href');
+        let linkurl1 = bgmedia.querySelector('.button .cmp-button[data-tracking-linkid*="primary button"]')?.getAttribute('href');
+        let linkurl2 = bgmedia.querySelector('.button .cmp-button[data-tracking-linkid*="ghost button"]')?.getAttribute('href');
+ 
         if (linkLabel1 && linkurl1) {
+            if (linkurl1 && !isAbsoluteUrl(linkurl1)) {
+                var currentDomain = window.location.origin;
+                linkurl1 = currentDomain + linkurl1;
+            }
             const backgroundMediaLinkData1 = [];
             const aTag = document.createElement('a');
             aTag.setAttribute('href', linkurl1);
@@ -209,6 +253,10 @@ const createBackgroundMedia = (main, document) => {
             backgroundMediaBlockData.push(backgroundMediaLinkData1);
         }
         if (linkLabel2 && linkurl2) {
+            if (linkurl2 && !isAbsoluteUrl(linkurl2)) {
+                var currentDomain = window.location.origin;
+                linkurl2 = currentDomain + linkurl2;
+            }
             const backgroundMediaLinkData2 = [];
             const aTag = document.createElement('a');
             aTag.setAttribute('href', linkurl2);
@@ -218,15 +266,15 @@ const createBackgroundMedia = (main, document) => {
             para.appendChild(node);
             backgroundMediaLinkData2.push(aTag, para);
             backgroundMediaBlockData.push(backgroundMediaLinkData2);
-
+ 
         }
-
+ 
         backgroundMediaBlock.push(backgroundMediaBlockData);
         console.log(backgroundMediaBlock);
         const backgroundMediaTable = WebImporter.DOMUtils.createTable(backgroundMediaBlock, document);
         main.append(backgroundMediaTable);
     });
-
+ 
 };
 
 const createAccordion = (main, document) => {
@@ -419,17 +467,17 @@ const createVideo = (main, document) => {
         if (videoElem.hasAttribute('data-src-large')) {
             deskVideoPath = videoElem.getAttribute('data-src-large');
         } else {
-            deskVideoPath = videoElem.getAttribute('src');
+			deskVideoPath = videoElem.getAttribute('src')?.replace('blob:', '');
         }
         if (videoElem.hasAttribute('data-src-medium')) {
             mobVideoPath = videoElem.getAttribute('data-src-medium');
         } else {
-            mobVideoPath = videoElem.getAttribute('src');
+            mobVideoPath = videoElem.getAttribute('src')?.replace('blob:', '');
         }
         const imgPath = video.querySelector('.cmp-video__video');
         const img = imgPath.querySelector('img');
-        const deskPosterImgPath = img.getAttribute('src');
-        const mobPosterImgPath = img.getAttribute('src');
+        const deskPosterImgPath = img?.getAttribute('src');
+		const mobPosterImgPath = img?.getAttribute('src');
         const loopVideo = 'TRUE';
         const autoplay = 'TRUE';
         const videoControls = 'FALSE';
@@ -509,7 +557,7 @@ const createDefaultContent = (main, document) => {
 
     });
 
-    // default buttons
+    // default buttonsmodeloverview
     const buttons = document.querySelectorAll('.button');
     const defaultButtons = Array.from(buttons).filter((div) =>!div.closest('.cmp-multi-content, .backgroundmedia, .carousel, .cmp-globalnavigation,[data-tracking-regionid*="item-image-text-teaser-right"], [data-tracking-regionid*="item-image-text-teaser-left"],[data-tracking-regionid*="footer"],[data-tracking-regionid*="modeloverview"]'));
     defaultButtons.forEach((button) => {
